abcfff532821be3efcb91621c0292bf61d42b947
Simon Yang <yzh503@live.com>
2023-09-20 12:57:07 +1000

    Add analytics

--- viz-app/app/layout.tsx
+++ viz-app/app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Sidebar from './Sidebar'
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -25,6 +26,7 @@ export default function RootLayout({
             {children}
           </div>
         </div>
+        <Analytics />
       </body>
     </html>
   )
