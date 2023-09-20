05f8b028e805709af0b5e75d1b1a0e3d0bec1463
Simon Yang <yzh503@live.com>
2023-09-20 13:00:59 +1000

    Fix build

--- viz-app/app/layout.tsx
+++ viz-app/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Sidebar from './Sidebar'
-import { Analytics } from '@vercel/analytics/react';
+import Analytics from '@vercel/analytics/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
